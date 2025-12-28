6f8df92216f4c9cb3a553aac5fcb2caaa2db0b68
Jordan Carroll <48207413+SurfJordan@users.noreply.github.com>
2025-12-28 19:55:22 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,6 @@ import { PLATFORM_NAME } from './settings.js';
  * This method registers the platform with Homebridge
  */
 export default (api: API) => {
+  console.log('🔥🔥🔥 TAPO PLUGIN ENTRY LOADED', __filename);
   api.registerPlatform(PLATFORM_NAME, TapoPlatform);
 };
